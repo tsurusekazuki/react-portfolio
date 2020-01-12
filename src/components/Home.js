1ab722bba582c33d0bb5e34fe277f2ca628ccaa1
tsurusekazuki <b1714935@planet.kanazawa-it.ac.jp>
2020-01-12 16:55:58 +0900

    add: home-page design

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,12 @@ class Home extends React.Component {
         return (
             <div className='wrapper'>
                 <div className='background_image'>
-
+                    <div className='home_block'>
+                        <h2 className='home_work'>Engineer</h2>
+                        <h1 className='home_username'>Kazuki Tsuruse</h1>
+                        <p>I am a student aiming to be a web engineer.</p>
+                        <p>I belong to the Smartphone App Project.</p>
+                    </div>  
                 </div>
             </div>
         )
