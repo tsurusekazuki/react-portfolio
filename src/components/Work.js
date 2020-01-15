9a348e6d8d5645df01d764eef6a17e4d5a52c2d2
tsurusekazuki <b1714935@planet.kanazawa-it.ac.jp>
2020-01-15 20:00:09 +0900

    add: work component config

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import '../css/Work.css'
+import WorkSkill from './WorkSkill';
 
 class Work extends React.Component {
     render() {
         return (
-            <h1>work</h1>
+            <div className='work_wrapper'>
+                <h1 className='work_title'>Work</h1>
+                <WorkSkill />
+            </div>
         )
     }
 }
